9a5a74e5b12089b762510c82a68f3e308915c2a9
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-10-07 13:27:57 +0200

    add trigger objects

--- byte_triggers/__init__.py
+++ byte_triggers/__init__.py
@@ -1,3 +1,6 @@
 from ._version import __version__  # noqa: F401
+from .lsl import LSLTrigger  # noqa: F401
+from .mock import MockTrigger  # noqa: F401
+from .parallel import ParallelPortTrigger  # noqa: F401
 from .utils.config import sys_info  # noqa: F401
 from .utils.logs import add_file_handler, logger, set_log_level  # noqa: F401
---
+++ byte_triggers/mock.py
@@ -0,0 +1,20 @@
+"""Mock trigger."""
+
+from ._base import BaseTrigger
+from .utils._docs import copy_doc
+from .utils.logs import logger
+
+
+class MockTrigger(BaseTrigger):
+    """Mock trigger class.
+
+    Delivered triggers are logged at the 'INFO' level.
+    """
+
+    def __init__(self):
+        pass
+
+    @copy_doc(BaseTrigger.signal)
+    def signal(self, value: int) -> None:
+        value = super().signal(value)
+        logger.info("Mock set to %i.", value)
